e5916bc7370fa34cf281a6b4440fcf16e0363d94
sharpsensoruser <41220204+sharpsensoruser@users.noreply.github.com>
2020-08-22 21:54:26 -0400

    Added version info and reset support.

--- foxconn_fstof2002c0u_demo/foxconn_fstof2002c0u_demo.ino
+++ foxconn_fstof2002c0u_demo/foxconn_fstof2002c0u_demo.ino
@@ -57,6 +57,8 @@ int readSerial() {
   return mySerial.read();
 }
 
+/////////////////////////////////////////////////////////////////////////////
+
 // Helper function to request distance measurement.
 void send_CMD_ST_MM() {
   // Send 1st Packet Header Byte.
@@ -65,7 +67,7 @@ void send_CMD_ST_MM() {
   // Send 2nd Packet Header Byte.
   sendSerial(0xAA);
 
-  // Send the Command Byte for measuring distance.
+  // Send the Command Code for measuring distance.
   sendSerial(TOFM_CMD_ST_MM);
 
   // Send the Data Length for this command in bytes.
@@ -106,6 +108,107 @@ void readDistance() {
 
 /////////////////////////////////////////////////////////////////////////////
 
+// Helper function to request version info.
+void send_CMD_RD_VERSION_INFO() {
+  // Send 1st Packet Header Byte.
+  sendSerial(0x55);
+
+  // Send 2nd Packet Header Byte.
+  sendSerial(0xAA);
+
+  // Send the Command Code for requesting version info.
+  sendSerial(TOFM_CMD_RD_VERSION_INFO);
+
+  // Send the Data Length for this command in bytes.
+  sendSerial(0);
+
+  // Send the Ending Packet Byte.
+  sendSerial(0xFA);
+}
+
+// Read the version info from the data packet.
+void readVersionInfo() {
+  // Read the Data Length.
+  int dataLength = readSerial();
+  if ( dataLength != 0x03 )
+    return;
+
+  // Read the sensor_ic type.
+  int sensor_ic = readSerial();
+  if ( sensor_ic == 0x02 ) {
+    Serial.println("sensor_ic = 0x02 (GP2AP02VT00F)");
+  } else if ( sensor_ic == 0x03 ) {
+    Serial.println("sensor_ic = 0x03 (GP2AP03VT00F)");
+  } else {
+    Serial.print("sensor_ic = 0x");
+    Serial.println(sensor_ic, HEX);    
+  }
+  
+  // Read the port type.
+  int port = readSerial();
+  if ( port == 0x41 ) {
+    Serial.println("port = 0x41('A') : Firmware supports UART and I2C");
+  } else if ( port == 0x49 ) {
+    Serial.println("port = 0x49('I') : Firmware supports I2C only");
+  } else if ( port == 0x55 ) {
+    Serial.println("port = 0x55('U') : Firmware supports UART only");    
+  } else {
+    Serial.print("port = 0x");
+    Serial.println(port, HEX);    
+  }
+
+  // Read the version number.
+  int vers = readSerial();
+  Serial.print("version = ");
+  Serial.println(vers);
+  
+  // Read the End Byte.
+  if ( readSerial() != 0xFA )
+    return;
+}
+
+/////////////////////////////////////////////////////////////////////////////
+
+// Send reset command.
+void send_CMD_RESET() {
+  // Send 1st Packet Header Byte.
+  sendSerial(0x55);
+
+  // Send 2nd Packet Header Byte.
+  sendSerial(0xAA);
+
+  // Send the Command Code for reset.
+  sendSerial(TOFM_CMD_RESET);
+
+  // Send the Data Length for this command in bytes.
+  sendSerial(0);
+
+  // Send the Ending Packet Byte.
+  sendSerial(0xFA);
+}
+
+/////////////////////////////////////////////////////////////////////////////
+
+void readDataPacket() {
+  // Look for 1st Packet Header Byte.
+  if ( readSerial() != 0x55 )
+    return;
+
+  // Look for 2nd Packet Header Byte.
+  if ( readSerial() != 0xAA )
+    return;
+
+  // Look for the Command Code.
+  int cmd = readSerial();
+  if ( cmd == TOFM_CMD_ST_MM ) {
+    readDistance();
+  } else if ( cmd == TOFM_CMD_RD_VERSION_INFO ) {
+    readVersionInfo();
+  }  
+}
+
+/////////////////////////////////////////////////////////////////////////////
+
 // Arduino setup function.
 void setup() {
   // Start the hardware serial port for the serial monitor.
@@ -117,8 +220,12 @@ void setup() {
   // Start the software serial port for communicating with FSTOF2002C0U.
   mySerial.begin(9600);
 
-  // Wait two seconds for startup.
-  delay(2000);
+  // Wait one second for startup.
+  delay(1000);
+
+  // Get version info.
+  send_CMD_RD_VERSION_INFO();
+  readDataPacket();
 }
 
 // Arduino main loop.
@@ -126,18 +233,7 @@ void loop() {
   // Send the request for a distance measurement.
   send_CMD_ST_MM();
   
-  // Look for 1st Packet Header Byte.
-  if ( readSerial() != 0x55 )
-    return;
-
-  // Look for 2nd Packet Header Byte.
-  if ( readSerial() != 0xAA )
-    return;
-
-  // Look for the Command for measuring distance.
-  int cmd = readSerial();
-  if ( cmd == TOFM_CMD_ST_MM ) {
-    readDistance();
-  }
+  // Read the data packet.
+  readDataPacket();
 
 } // END PROGRAM
