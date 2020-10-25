52b1d53ff570bf661e45a9c33a6f5135296e74ce
sharpsensoruser <41220204+sharpsensoruser@users.noreply.github.com>
2020-10-25 18:17:04 -0400

    Rename app exe.

--- GP2Y1040AU0F/sharp_gp2y1040au0f_demo_csharp/SharpDustSensorDemo/Properties/AssemblyInfo.cs
+++ GP2Y1040AU0F/sharp_gp2y1040au0f_demo_csharp/SharpDustSensorDemo/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("SharpMicrowaveSensorDemo")]
+[assembly: AssemblyTitle("SharpDustSensorDemo")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("SharpMicrowaveSensorDemo")]
-[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyProduct("SharpDustSensorDemo")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
