ed75357a6f35d6ffc0c4dfb304166a8642a4a417
sharpsensoruser <41220204+sharpsensoruser@users.noreply.github.com>
2020-10-27 15:42:14 -0400

    Added read and send command data.

--- GP2Y1040AU0F/sharp_gp2y1040au0f_demo_uart.ino
+++ GP2Y1040AU0F/sharp_gp2y1040au0f_demo_uart.ino
@@ -3,8 +3,8 @@
 //
 // Board Connection:
 //   Arduino       GP2Y1040AU0F
-//   10pin         Pin 1 - TXD
-//   Not Connected Pin 2 - RXD
+//   10pin         Pin 1 - TXD      Use 3.3V logic level converter
+//   11pin         Pin 2 - RXD      Use 3.3V logic level converter
 //   Not Connected Pin 3 - SEL
 //   GND           Pin 4 - GND
 //   5V            Pin 5 - VCC
@@ -16,6 +16,12 @@
 
 #include <SoftwareSerial.h>
 
+// Test sending some commands.
+//#define TEST_SEND_CMDS
+#ifdef TEST_SEND_CMDS
+static int counter = 0;
+#endif // TEST_SEND_CMDS
+
 // Use software serial port for communicating with GP2Y1040AU0F.
 // If you have Arduino Mega 2560, you can use hardware serial port
 // instead like Serial1, etc.
@@ -24,10 +30,12 @@
 static SoftwareSerial mySerial(rxPin, txPin);
 
 // Starting bytes of UART data record.
-#define StartFrame1 0xFF
-#define StartFrame2 0xFA
-
-// Store UART data record raw bytes and calculated field values.
+#define START_FRAME_1 0xFF
+#define START_FRAME_2 0xFA
+#define CMD_FRAME_1   0x42
+#define CMD_FRAME_2   0x4D
+        
+// Store UART raw data frame bytes and calculated field values.
 static uint8_t frames[28];
 static int NC_03;  // Number concentration of particles with size 0.3um to 10um
 static int NC_05;  // Number concentration of particles with size 0.5um to 10um
@@ -59,35 +67,38 @@ void printValue(String text, int value, bool isLast = false) {
   }
 }
 
-// Read the 28-byte UART data record.
-void readDataRecord() {
-  // Look for the 1st start frame.  
-  frames[0] = readSerial();
-  if ( frames[0] != StartFrame1 )
-    return;
+/////////////////////////////////////////////////////////////////////////////
+
+// Read the remainder of the 28-byte UART air quality data record.
+void readAirQualityData() {
+  // Set the 1st start frame since we already read it.
+  frames[0] = START_FRAME_1;
   
   // Look for the 2nd start frame.
-  frames[1] = readSerial();
-  if ( frames[1] != StartFrame2 )
+  int data = readSerial();
+  if ( data != START_FRAME_2 )
     return;
-    
+  frames[1] = (uint8_t)data;
+  
   // Read the next 26 frames/bytes.
   for(int i = 2; i < 28; i++) {
-    frames[i] = readSerial();    
+    int data = readSerial();
+    if (data < 0 || data > 255)
+      return;
+    frames[i] = (uint8_t)data;    
   }
 
-  // Print raw data frames.
-  Serial.print("Raw data 00 to 13: 0x");
-  Serial.print(frames[0], HEX);
-  for(int i = 1; i < 14; i++) {
-    Serial.print(", 0x");
+  // Print raw air quality data frames.
+  Serial.println("---");
+  Serial.print("Read Air Data (HEX):");
+  for(int i = 0; i < 14; i++) {
+    Serial.print(" ");
     Serial.print(frames[i], HEX);
   }
   Serial.println("");
-  Serial.print("Raw data 14 to 27: 0x");
-  Serial.print(frames[14], HEX);
-  for(int i = 15; i < 28; i++) {
-    Serial.print(", 0x");
+  Serial.print("Read Air Data (HEX):");
+  for(int i = 14; i < 28; i++) {
+    Serial.print(" ");
     Serial.print(frames[i], HEX);
   }
   Serial.println("");
@@ -98,7 +109,7 @@ void readDataRecord() {
     checksum += frames[i];
   }
   if ( checksum != frames[27] ) {
-    Serial.print("Checksum doesn't match: calculated sum is: 0x");
+    Serial.print("Error: Checksum doesn't match: calculated sum is: 0x");
     Serial.println(checksum, HEX);
     return;
   }
@@ -125,11 +136,85 @@ void readDataRecord() {
   Serial.println("");
   printValue("PM1_1",  PM1_1);
   printValue("PM25_1", PM25_1);
-  printValue("PM10_1", PM10_1);
+  printValue("PM10_1", PM10_1, true);
+  Serial.println("");
   printValue("PM1_2",  PM1_2);
   printValue("PM25_2", PM25_2);
   printValue("PM10_2", PM10_2, true);
   Serial.println("");
+}
+
+// Read the rest of command record following the first cmd frame.
+void readCommandData()
+{
+  // Set the 1st cmd frame since we already read it.
+  frames[0] = CMD_FRAME_1;
+
+  // Look for the 2nd cmd frame.
+  int data = readSerial();
+  if ( data != CMD_FRAME_2 )
+    return;
+  frames[1] = (uint8_t)data;
+  
+  // Read in 5 remaining bytes from serial.
+  for(int i = 2; i < 7; i++)
+  {
+    int data = readSerial();
+    if (data < 0 || data > 255)
+      return;
+    frames[i] = (uint8_t)data;
+  }
+
+  // Print out the 7 bytes of cmd data.
+  Serial.println("---");
+  Serial.print("Read Cmd Data (HEX):");
+  for(int i = 0; i < 7; i++)
+  {
+    Serial.print(" ");
+    Serial.print(frames[i], HEX);
+  }
+  Serial.println("");
+}
+
+// Send UART command data to sensor.
+//
+// Examples of the 7-bytes of commmand data:
+//   READ: Read data in passive mode: 42 4D E2 00 00 01 71
+//   MODE: Switch to passive mode: 42 4D E1 00 00 01 70
+//   MODE: Switch to active mode: 42 4D E1 00 01 01 71
+//   SLEEP: Go to sleep: 42 4D E4 00 00 01 73
+//   SLEEP: Wake up: 42 4D E4 00 01 01 74
+//   CLEAN: Start cleaning: 42 4D 01 00 00 00 90
+//   RESET: Software reset: 42 4D 02 81 00 01 12
+//   MAVE: Set number of moving averages to 10: 42 4D 03 00 0A 00 9C
+//   TINT: Set interval time to 0 seconds: 42 4D 04 00 00 00 93
+//   TPREFAN: Set pre-rotation time of fan to 3 seconds: 42 4D 05 00 03 00 97
+//   TINTC: Set interval time for auto - cleaning to 60480[10s]: 42 4D 06 EC 40 01 C1
+//   TCLEAN: Set cleaning time to 10 seconds: 42 4D 07 00 0A 00 A0      
+void sendCommandData(uint8_t cmd, uint16_t dataValue)
+{
+  uint8_t buf[7];
+  buf[0] = CMD_FRAME_1;
+  buf[1] = CMD_FRAME_2;
+  buf[2] = cmd;
+  buf[3] = (uint8_t)((dataValue & 0xFF00) >> 8);
+  buf[4] = (uint8_t)(dataValue & 0x00FF);
+  uint16_t checksum = buf[0] + buf[1] + buf[2] + buf[3] + buf[4];
+  buf[5] = (uint8_t)((checksum & 0xFF00) >> 8);
+  buf[6] = (uint8_t)(checksum & 0x00FF);
+
+  // Print out the cmd data before sending.
+  Serial.println("---");
+  Serial.print("Send Cmd Data (HEX):");
+  for(int i = 0; i < 7; i++)
+  {
+    Serial.print(" ");
+    Serial.print(buf[i], HEX);
+  }
+  Serial.println("");
+
+  // Send cmd data to sensor.
+  mySerial.write(buf, 7);
 }
 
 /////////////////////////////////////////////////////////////////////////////
@@ -142,7 +227,7 @@ void setup() {
   Serial.println("GP2Y1040AU0F Demo");
   Serial.println("=================");
 
-  // Start the software serial port for receiving data from GP2Y1040AU0F.
+  // Start the software serial port for sending and receiving data from GP2Y1040AU0F.
   mySerial.begin(9600);
 
   // Wait a second for startup.
@@ -151,9 +236,30 @@ void setup() {
 
 // Arduino main loop.
 void loop() {
-  // Read data record.
-  readDataRecord();
-  Serial.println("---");
+  // Test sending some cmds.
+  #ifdef TEST_SEND_CMDS
+  counter++;
+  if ( counter == 10 ) {
+    sendCommandData(0xE1, 0);    // Switch to passive mode
+    sendCommandData(0x03, 10);   // Set number of moving averages to 10    
+    sendCommandData(0xE2, 0);    // Read air data in passive mode    
+    sendCommandData(0x01, 0);    // Start cleaning
+    sendCommandData(0xE1, 1);    // Switch to active mode
+    return;  
+  }
+  #endif // TEST_SEND_CMDS
+  
+  // Read frame and handle accordingly.
+  int frame = readSerial();
+  if (frame == START_FRAME_1)
+  {
+    readAirQualityData();
+  }
+  else if (frame == CMD_FRAME_1)
+  {
+    readCommandData();
+  }
+  
   delay(100);
   
 } // END PROGRAM
